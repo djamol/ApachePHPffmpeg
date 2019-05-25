3caf6ac165f273a5afd16054d38ee1063058e78e
DjAmolGroup <djamolpatil@gmail.com>
2019-05-25 18:30:58 +0530

    Update latest.sh

--- latest.sh
+++ latest.sh
@@ -1,3 +1,3 @@
 yum -y install wget unzip;
-wget --no-check-certificate -O installer.zip https://github.com/djamol/ApachePHPffmpeg/archive/master.zip; 
-unzip installer.zip; cd ApachePHPffmpeg-master;chmod +x autoinstall;chmod +x inc/*.sh; ./autoinstall 
+wget --no-check-certificate -O installer.zip https://github.com/djamol/ApachePHPffmpeg/archive/amol.zip; 
+unzip installer.zip; cd ApachePHPffmpeg-amol;chmod +x autoinstall;chmod +x inc/*.sh; ./autoinstall 
