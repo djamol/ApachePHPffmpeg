f924e6402fa84e53528c9a133e2ba18766c817f7
DjAmolGroup <djamolpatil@gmail.com>
2019-05-26 13:32:00 +0530

    Update last.sh

--- inc/last.sh
+++ inc/last.sh
@@ -17,7 +17,7 @@ chmod -R g+rx /home/www/;chmod -R g+rwx /home/www/
 #Auto Start Apache2 on boot/reboot server
 #touch /etc/init.d/apache2
 #echo -e '#!/bin/bash\n# apache2-Startup script for the Apache HTTP Server\n/usr/local/apache2/bin/apachectl $@' >> /etc/init.d/apache2
-cp -f $SCRIPTPATH/apache2 /etc/init.d/apache2
+cp -f $SCRIPTPATH/conf/apache2 /etc/init.d/apache2
 chmod 755 /etc/init.d/apache2
 chkconfig --add apache2
 chkconfig --list apache2
@@ -51,6 +51,14 @@ echo named -v
 echo curl -V
 #OPEN SSL VERSION
 echo openssl version
+
+#AmolPrograms
+mkdir -p /usr/amol/bin
+export PATH="$PATH:/usr/amol/bin"
+cp -f $SCRIPTPATH/pgrogram/addftp /usr/amol/bin/addftp
+
+chmod -R +x /usr/amol/bin
+
 
 cp -f $SCRIPTPATH/conf/apache.conf /etc/webmin/apache/config
 cp -f $SCRIPTPATH/conf/httpd.conf /usr/local/apache2/conf/httpd.conf
