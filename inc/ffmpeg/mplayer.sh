f263178f40ceaf934c8ef43dc271154ff3e3f54d
DjAmolGroup <djamolpatil@gmail.com>
2019-06-08 11:31:54 +0530

    Create mplayer.sh

---
+++ inc/ffmpeg/mplayer.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+# Amol Patil
+#Email US :support@djamol.com
+ # WEB@DjAmol.com
+_package='MPlayer-1.3.0.tar.xz'
+clear
+sleep 2
+echo -e '
+ ▒█░▒░▓█░█▒░▒▒█░█▓░▒░█▓   █████▒    ▓█ ▒█▓▓█▓ ██████ ▓████▒░█░      ░█████░▒████▓ ▓█████░ 
+ ░▓ █▓▓█ ▓ ██▒█░▓░▓█░█▓   ▓▒  █▓    ▒█ ▒▓▓▓█▓ ▓░█▓▒█░▓▒  █▓ ▓       ░█░  ▓ ▒▓  ▓█ ▓▒▓█▒█▒ 
+ ▒█▒█▓▓█░█▓▓█▒█▒█▓▓█▒█▓   █▓  █▓░▓░ ▓█ ██▒▒█▓░█▒█▓▒█░██  █▓░█▒  ░   ▒█▒ ░▓ ██  ██ █▓▓█░█▒ 
+ ░█████▓ ██████ ▓█████░▓█ █████░░█████ █▓  █▓░█▒░░▒█ ▓████▒░█████▒█▓ █████ ▒████▓ █▓░▒░█▒ 
+';
+echo -e "$GREEN 
+# Powered By DjAmol Group Inc         #
+# Email : Support@djamol.com          #
+# Website : www.djamol.com            #
+# Copyright By PatilWeb.com           #
+# IG:PatilWeb FB:PatilWeb TW:PatilWeb #
+$RESET";
+
+echo -e $RED"Installation of $_package ....... started"$RESET
+
+cd $INSTALL_SDIR/
+rm -rf mplayer*
+   cd $INSTALL_SDIR
+
+if [ ! -f $SCRIPTPATH/src/ffmpeg/$_package ]
+then
+   echo -e "\033[33;34m file " $_package " does not exist.";
+
+		if wget -O  $_package $SOURCE_URL/$_package; then 
+		date +"%r" >> $BUILD;echo "Success : File Download $_package" >> $BUILD
+		else
+		date +"%r" >> $BUILD;echo "Failed : File Download $_package" >> $BUILD
+		fi
+else
+		cp $SCRIPTPATH/src/ffmpeg/$_package $INSTALL_SDIR/
+		echo -e "\033[33;32m file " $_package " exists.";date +"%r" >> $BUILD;echo "Status : Already Exist $_package" >> $BUILD
+fi
+
+tar -xvJf $_package
+#/usr/local/avpffmpeg/bin/svn  checkout  svn://svn.mplayerhq.hu/mplayer/trunk mplayer
+#cd mplayer/
+cd MPlayer-1.3.0/
+./configure --prefix=$INSTALL_DDIR  --codecsdir=$INSTALL_DDIR/lib/codecs/   \
+		--extra-cflags=-I/usr/local/avpffmpeg/include/ --extra-ldflags=-L/usr/local/avpffmpeg/lib \
+		--with-freetype-config=/usr/local/avpffmpeg/bin/freetype-config   --yasm=/usr/local/avpffmpeg/bin/yasm --disable-gui
+make -j$cpu
+if   make install; then
+date +"%r" >> $BUILD;echo "Succcess :MPlayer Installled" >> $BUILD;
+echo -e $RED" MPlayer Installed Success ......"$RESET
+else
+date +"%r" >> $BUILD;echo "Failed :MPlayer Installation Failed" >> $BUILD;
+echo -e $RED"Failed :MPlayer Installation Failed ......"$RESET
+fi
+cp -f etc/codecs.conf $INSTALL_DDIR/etc/mplayer/codecs.conf
+ln -sf /usr/local/avpffmpeg/bin/mplayer /usr/local/bin/mplayer
+ln -sf /usr/local/avpffmpeg/bin/mplayer /usr/bin/mplayer
+ln -sf /usr/local/avpffmpeg/bin/mencoder /usr/bin/mencoder
+ln -sf /usr/local/avpffmpeg/bin/mencoder /usr/local/bin/mencoder
+echo -e $RED"
+                        :rvri                     
+                  :vJUJYri:::::i::                
+            .sDgEJi                 r7i           
+         .KQX7.                        .L:        
+       iRRs.        5vsvv7v7Yv            Kr      
+     :gd5.         5Yv7LYL777I.             M     
+    PbuI  ..      ru7rrv:77rrr5              Q7   
+   QUYI. .        I7r7vr .srir7j              BJ  
+  g2LYj          ULr7rS   17rirJ:              Rr 
+  EY7JL         i1r7rj:    2rrirU              KS 
+ sUvvvI.        57rr7U     7LrrivJ             r1.
+.1sv7vss       2v7r7J       sriiiJ:            r7:
+:jjv77r7L     ru777vY       :7rirr2            7i:
+ uJvriBMvI:   KL7vr:vuUIUI1urririrru           :r:
+  Zvi1BBEiK2i:ii7:...jI2IUI11Lririrvr         ..X 
+  gJ5BYB:i.iUYr:..rJ:         Jririrv7        .ur 
+   DZQBu : 7Yvv77777j.        iYriiiirjr:...:iUY  
+    5q: ...v:7Ls7v7uvU:        is7iriri77vvvvSr   
+     :SYJ:::::. Iqr :7U.        .rLrrrrirrrsU     
+       vq1vri.  Er   u75.         .::iirrJJ.      
+         1JvJv.Ii  rBB:.i.    . .   ..r7i         
+        LRbbPZbEKI .BR   s:....:ir7Lr:            
+                  :......ir.....                 
+"$RESET
+
+echo -e $RED"Installation of $_package ....... Completed"$RESET
+sleep 2
