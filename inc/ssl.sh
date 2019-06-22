db2012cbba4a061583fc7bf1754c5c1b6a5f9bd2
DjAmolGroup <djamolpatil@gmail.com>
2019-06-22 08:01:15 -0700

    Update ssl.sh

--- inc/ssl.sh
+++ inc/ssl.sh
@@ -1,4 +1,22 @@
+#_file="$1"
+#[ $# -eq 0 ] && { echo "Usage: $0 filename"; exit 1; }
+#[ ! -f "$_file" ] && { echo "Error: $0 file not found."; exit 2; }
+#if [ -s "$_file" ] && [ -f "$_file" ]
+#then
+#        echo "$_file has some data."
+        # do something as file has data
+#else
+#        echo "$_file is empty."
+        # do something as file is empty
+#fi
 CAC="US"; CAS="New York"; CAL="New York City"; CAO="Private ORG"; CAU="DA Group"; CACM="serv.djamol.com"; CAM="support@djamol.com";
+mkdir -p /usr/share/ssl/certs
+mkdir -p /etc/ssl/private
+_file="/etc/ssl/certs/postfix.pem"
+if [ -s "$_file" ] && [ -f "$_file" ] then
+        echo "$_file ssl certificate Exist."
+else
+        echo "$_file certificate file is empty or not found."
 SSLCERt=$(expect -c "
 set timeout 35
   spawn openssl req -new -x509 -days 3650 -nodes -out /etc/ssl/certs/postfix.pem -keyout /etc/ssl/private/postfix.pem
@@ -19,6 +37,14 @@ set timeout 35
 expect eof
 ")
 echo "$SSLCERt"
+fi
+
+_file="/usr/share/ssl/certs/amol.cert"
+if [ -s "$_file" ] && [ -f "$_file" ] then
+        echo "$_file ssl certificate Exist."
+else
+        echo "$_file certificate file is empty or not found."
+
 SSLCERt=$(expect -c "
 set timeout 35
   spawn openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /usr/share/ssl/certs/amol.key -out /usr/share/ssl/certs/amol.cert
@@ -40,6 +66,14 @@ set timeout 35
 expect eof
 ")
 echo "$SSLCERt"
+fi
+
+
+_file="/usr/share/ssl/certs/ssl.crt"
+if [ -s "$_file" ] && [ -f "$_file" ] then
+        echo "$_file ssl certificate Exist."
+else
+        echo "$_file certificate file is empty or not found."
 
 SSLHTTPS=$(expect -c "
 set timeout 35
@@ -62,3 +96,4 @@ set timeout 35
 expect eof
 ")
 echo "$SSLHTTPS"
+fi
