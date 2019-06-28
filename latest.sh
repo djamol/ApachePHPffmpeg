87744ee8007348659cd35ab874f155487fa62756
DjAmolGroup <djamolpatil@gmail.com>
2019-06-28 21:48:46 +0530

    Delete latest.sh

--- latest.sh
+++
@@ -1,3 +0,0 @@
-yum -y install wget unzip;
-wget --no-check-certificate -O installer.zip https://github.com/djamol/ApachePHPffmpeg/archive/amol.zip; 
-unzip installer.zip; cd ApachePHPffmpeg-amol;chmod +x autoinstall;chmod +x inc/*.sh; ./autoinstall 
