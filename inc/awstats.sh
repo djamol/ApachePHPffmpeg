52a0956336400ba624c74a85fa2b50ac0eb459a6
DjAmolGroup <djamolpatil@gmail.com>
2019-07-05 19:09:35 +0530

    Update awstats.sh

--- inc/awstats.sh
+++ inc/awstats.sh
@@ -22,8 +22,8 @@ echo "$SSLCERt"
 
 
 cp /etc/awstats/awstats.localhost.localdomain.conf /etc/awstats/awstats.$MAINDOMAIN.conf
-sed -i 's/localhost.localdomain/$MAINDOMAIN/' /etc/awstats/awstats.$MAINDOMAIN.conf
- sed -i 's/^HostAliases.*/HostAliases="loalhost\ 127.0.0.1\ $MAINDOMAIN"/' /etc/awstats/awstats.$MAINDOMAIN.conf
+sed -i "s/localhost.localdomain/$MAINDOMAIN/" /etc/awstats/awstats.$MAINDOMAIN.conf
+ sed -i "s/^HostAliases.*/HostAliases="loalhost\ 127.0.0.1\ $MAINDOMAIN"/" /etc/awstats/awstats.$MAINDOMAIN.conf
 /usr/share/awstats/wwwroot/cgi-bin/awstats.pl -config=$MAINDOMAIN -update
 ## cronjob update daily
 0 * * * * root /usr/share/awstats/wwwroot/cgi-bin/awstats.pl -config=$MAINDOMAIN -update
