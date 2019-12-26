06d5ae9200ca8e37543a4f635936313fdfea14c3
DjAmolGroup <djamolpatil@gmail.com>
2019-12-26 22:18:47 +0530

    Create vnstat.sh

---
+++ inc/vnstat.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+##vnstat 
+yum -y install vnstat
+if [ -f /usr/bin/vnstat ] 
+then
+        for int in `vnstat --iflist| sed 's/Available interfaces://'` 
+        do 
+                echo " INFO: Initialising vnstat db for $int"
+                vnstat -u -i $int
+        done
+
+        chown vnstat.vnstat  `cat /etc/vnstat.conf | grep DatabaseDir |sed 's/"//g' |awk '{print $2 "/*"}'`
+
+	chkconfig vnstat on
+        service vnstat restart
+fi
