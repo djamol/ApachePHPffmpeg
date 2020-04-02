297f168f12199f3e41c8907cd6549c45acc0d6c6
Patil Technologies <djamolpatil@gmail.com>
2020-04-02 11:24:26 +0530

    Update ftp.sh

--- inc/ftp.sh
+++ inc/ftp.sh
@@ -7,6 +7,17 @@
 #rpm -ivh http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-8.noarch.rpm
 rpm -ivh http://ftp.astral.ro/mirrors/fedora/pub/epel/epel-release-latest-$OSV.noarch.rpm
 yum -y install proftpd proftpd-utils
+
+
+groupadd ftpgroup
+useradd -G ftpgroup djamoltest -s /sbin/nologin -d /home/www2
+#passwd djamol
+chmod -R 1750 /home/www2
+setsebool -P allow_ftpd_full_access=1
+openssl req -x509 -nodes -newkey rsa:1024 -keyout /etc/pki/tls/certs/proftpd.pem -out /etc/pki/tls/certs/proftpd.pem
+chmod  0440 /etc/pki/tls/certs/proftpd.pem
+systemctl restart proftpd.service
+
 cp -f $SCRIPTPATH/conf/proftpd.conf /etc/proftpd.conf
  mkdir /etc/proftpd; touch /etc/proftpd/passwd.vhosts;chmod 640 /etc/proftpd/passwd.vhosts
 cp -f $SCRIPTPATH/conf/ftpd-rsa.pem /etc/proftpd/ftpd-rsa.pem
