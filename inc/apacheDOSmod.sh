53d9a88df37ec3f457adf1a9b6895cf3d22f480e
Patil Technologies <djamolpatil@gmail.com>
2021-05-10 19:42:31 +0530

    mod_evasive24 for Apache DDOS protection mod

---
+++ inc/apacheDOSmod.sh
@@ -0,0 +1,20 @@
+
+#Install CSF
+#cd $SCRIPTPATH
+#rm -fv csf.tgz
+cd $SCRIPTPATH/src
+FILE=mod_evasive.tar.gz
+if [ ! -f $FILE ]
+then
+		echo -e "\033[33;34m file " $FILE " does not exist.";date +"%r" >> $BUILD;echo "Status : Downloading $FILE" >> $BUILD
+		###
+else
+		echo -e "\033[33;32m file " $FILE " exists.";date +"%r" >> $BUILD;echo "Status : Exist $FILE" >> $BUILD
+fi
+
+tar -xzf mod_evasive.tar.gz
+cd mod_evasive*
+/usr/local/apache2/bin/apxs -i -a -c mod_evasive24.c
+echo -e '\n<IfModule mod_evasive24.c>\nDOSHashTableSize 3097\nDOSPageCount 2\nDOSSiteCount 50\nDOSPageInterval 1\nDOSSiteInterval 1\nDOSBlockingPeriod 60\nDOSEmailNotify mramolpatil1@gmail.com\n</IfModule>' >> /usr/local/apache2/conf/httpd.conf
+service apache2 restart
+date +"%r" >> $BUILD;echo "mod_evasive DOS Apache Program Complete" >> $BUILD
