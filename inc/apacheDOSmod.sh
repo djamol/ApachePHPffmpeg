2cf1493a26689a6c3f62869ce81da1ba62a341c1
Patil Technologies <djamolpatil@gmail.com>
2021-05-10 19:45:10 +0530

    Update apacheDOSmod.sh

--- inc/apacheDOSmod.sh
+++ inc/apacheDOSmod.sh
@@ -17,4 +17,6 @@ cd mod_evasive*
 /usr/local/apache2/bin/apxs -i -a -c mod_evasive24.c
 echo -e '\n<IfModule mod_evasive24.c>\nDOSHashTableSize 3097\nDOSPageCount 2\nDOSSiteCount 50\nDOSPageInterval 1\nDOSSiteInterval 1\nDOSBlockingPeriod 60\nDOSEmailNotify mramolpatil1@gmail.com\n</IfModule>' >> /usr/local/apache2/conf/httpd.conf
 service apache2 restart
+#test mod
+#perl test.pl
 date +"%r" >> $BUILD;echo "mod_evasive DOS Apache Program Complete" >> $BUILD
