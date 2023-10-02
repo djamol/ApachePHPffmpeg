e9972920788f5d6abdcc9bd500cfe67c30593408
Amol Patil <djamolpatil@gmail.com>
2023-10-02 19:02:05 +0530

    Yum to APT

--- latest.sh
+++ latest.sh
@@ -28,6 +28,6 @@ if [ "$ARCH" = "x86_64" ]; then
   echo -e "------> CONFIGURATION FOR 64-BIT ONLY."  
 ##  exit 1
 fi
-yum -y install wget unzip;
+apt -y install wget unzip;
 wget --no-check-certificate -O installer.zip https://github.com/djamol/ApachePHPffmpeg/archive/amol.zip; 
 unzip installer.zip; cd ApachePHPffmpeg-amol;chmod +x autoinstall;chmod +x inc/*.sh; ./autoinstall 
